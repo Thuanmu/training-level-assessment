1311a6afc3f77817e0dfac98f6a5a8cc1ca4d97a
Thuanmu <43351896+Thuanmu@users.noreply.github.com>
2021-03-04 15:16:15 +0700

    Initial commit

---
+++ src/test/java/com/thuanmu/traininglevelassessment/TrainingLevelAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuanmu.traininglevelassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingLevelAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
