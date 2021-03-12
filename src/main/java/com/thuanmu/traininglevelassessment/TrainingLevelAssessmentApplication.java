3c892e831b6f3eeb50a442db45abb06332646084
Thuanmu <43351896+Thuanmu@users.noreply.github.com>
2021-03-12 17:27:25 +0700

    update configuration

--- src/main/java/com/thuanmu/traininglevelassessment/TrainingLevelAssessmentApplication.java
+++ src/main/java/com/thuanmu/traininglevelassessment/TrainingLevelAssessmentApplication.java
@@ -1,23 +1,14 @@
 package com.thuanmu.traininglevelassessment;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
-@EnableAutoConfiguration
 public class TrainingLevelAssessmentApplication {
 	
-	@RequestMapping("/")
-	String home() {
-		return "Xin chao thuan";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingLevelAssessmentApplication.class, args);
+		
 	}
 
 }
