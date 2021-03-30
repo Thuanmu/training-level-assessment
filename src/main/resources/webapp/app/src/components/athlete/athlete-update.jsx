134b3b3eb77d359d534bf1fb205aae8e1d30c371
Thuanmu <43351896+Thuanmu@users.noreply.github.com>
2021-03-30 12:13:27 +0700

    Add functions to add, edit, delete and automatically create time for athlete

---
+++ src/main/resources/webapp/app/src/components/athlete/athlete-update.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from "react";
+import {Button, Col, Container, Form, FormGroup, Input, Label, Row} from "reactstrap";
+import { Link } from 'react-router-dom';
+// import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
+import AthleteService from "./athlete-service";
+import NowDateTime from "../../utilities/now-date-time";
+import moment from "moment";
+
+export default function AthleteUpdate(props) {
+
+    const [athleteId, setAthleteId] = useState(props.match.params.id);
+    const [athleteName, setAthleteName] = useState('');
+    const [dateOfBirth, setDateOfBirth] = useState('');
+    const [hometown, setHometown] = useState('');
+    const [totalScoresOfCriterias, setTotalScoresOfCriterias] = useState('');
+    const [grade, setGrade] = useState('');
+    const [athleteRank, setAthleteRank] = useState('');
+    const [createAt, setCreateAt] = useState('');
+    const [lastModified, setLastModified] = useState('');
+
+    const handleAthleteId = event => setAthleteId(props.match.params.id);
+    const handleAthleteName = event => setAthleteName(event.target.value);
+    const handleDateOfBirth = event => setDateOfBirth(event.target.value);
+    const handleHometown = event => setHometown(event.target.value);
+    const handleTotalScoresOfCriterias = event => setTotalScoresOfCriterias(event.target.value);
+    const handleGrade = event => setGrade(event.target.value);
+    const handleAthleteRank = event => setAthleteRank(event.target.value);
+    
+    useEffect(() => {
+        if(athleteId)  {
+            AthleteService.getAthleteById(athleteId).then( res => {
+                let athlete = res.data;
+                setAthleteId(athlete.id);
+                setAthleteName(athlete.athleteName);
+                setDateOfBirth(athlete.dateOfBirth);
+                setHometown(athlete.hometown);
+                setTotalScoresOfCriterias(athlete.totalScoresOfCriterias);
+                setGrade(athlete.grade);
+                setAthleteRank(athlete.athleteRank);
+                setCreateAt(athlete.createAt);
+                setLastModified(athlete.lastModified);
+            });
+        }
+    },[]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let datetime = NowDateTime.getNowDateTime();
+    
+        if(!athleteId) {
+            let athlete = {
+                athleteName: athleteName,
+                dateOfBirth: moment(dateOfBirth).format("DD-MM-YYYY"),
+                hometown: hometown,
+                totalScoresOfCriterias: totalScoresOfCriterias,
+                grade: grade,
+                athleteRank: athleteRank,
+                createAt: datetime,
+                lastModified: datetime
+            };
+
+            AthleteService.createAthlete(athlete).then(res => {
+                props.history.push('/athletes');
+            });
+        } 
+        else {
+            let athlete = {
+                id: athleteId,
+                athleteName: athleteName,
+                dateOfBirth: moment(dateOfBirth).format("DD-MM-YYYY"),
+                hometown: hometown,
+                totalScoresOfCriterias: totalScoresOfCriterias,
+                grade: grade,
+                athleteRank: athleteRank,
+                createAt: createAt,
+                lastModified: datetime
+            };
+
+            AthleteService.updateAthlete(athlete, athleteId).then( res => {
+                props.history.push('/athletes');
+            });
+        }
+
+    }
+
+    
+    const title = <h2>{ athleteId ? "Sửa Vận động viên" : "Thêm Vận động viên" }</h2>;
+            
+    return(
+        <div>
+            <Container>
+                {title}
+                <Form onSubmit={handleSubmit}>
+                    {/* <FormGroup>
+                        <Label for="athlete-id">Id</Label>
+                        <Input type="text" name="athlete-id" id="athlete-id" value={athleteId} onChange={handleAthleteId} />
+                    </FormGroup> */}
+                    <FormGroup>
+                        <Label for="athlete-name">Tên Vận động viên</Label>
+                        <Input type="text" name="athlete-name" id="athlete-name" value={athleteName} onChange={handleAthleteName} required />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="date-of-birth">Ngày sinh</Label>
+                        <Input type="date" name="date-of-birth" id="date-of-birth" value={dateOfBirth} onChange={handleDateOfBirth} required />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="home-town">Quê quán</Label>
+                        <Input type="text" name="name" id="name" value={hometown} onChange={handleHometown} required />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="total-scores-of-criterias">Tổng điểm</Label>
+                        <Input type="text" name="total-scores-of-criterias" id="total-scores-of-criterias" value={totalScoresOfCriterias} onChange={handleTotalScoresOfCriterias} readOnly />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="grade">Xếp loại</Label>
+                        <Input type="text" name="grade" id="grade" value={grade} onChange={handleGrade} readOnly />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="athlete-rank">Xếp hạng</Label>
+                        <Input type="text" name="athlete-rank" id="athlete-rank" value={athleteRank} onChange={handleAthleteRank} readOnly />
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Lưu</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/athletes">Hủy</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    );
+
+}
---
+++ src/main/resources/webapp/app/src/components/athlete/athlete.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import {Button, ButtonGroup, Col, Container, Row, Table} from "reactstrap";
+import AthleteService from "./athlete-service";
+
+export default function Athlete(props) {
+
+    const [athletes, setAthletes] = useState([]);
+
+    const addAthlete = () => {
+        props.history.push(`/athletes/new`);
+    }
+
+    const viewAthlete = id => {
+        props.history.push(`/athletes/${id}/detail`);
+    }
+    
+    const editAthlete = id => {
+        props.history.push(`/athletes/${id}/edit`);
+    }
+
+    const deleteAthlete = id => {
+        AthleteService.deleteAthlete(id).then( (res) => {
+         setAthletes(athletes.filter(athlete => athlete.id !== id));
+        });
+    }
+
+    useEffect(() => {
+        AthleteService.getAthletes().then((res) => {
+            setAthletes(res.data);
+        });
+    },[]);
+
+    return(
+        <div>
+            <Container>
+                <h2>
+                    <Row>
+                        <Col md="5">Danh sách Vận động viên</Col>
+                        <Col md="5"></Col>
+                        <Col md="2">
+                            <Button size="sm" color="success" onClick={addAthlete}>Thêm Vận động viên</Button>
+                        </Col>
+                    </Row>
+                </h2>
+                &nbsp;
+                <Table responsive hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tên vận động viên</th>
+                            <th>Ngày sinh</th>
+                            <th>Quê quán</th>
+                            <th>Tổng điểm</th>
+                            <th>Xếp loại</th>
+                            <th>Xếp hạng</th>
+                            <th>Ngày tạo</th>
+                            <th>Cập nhật lần cuối</th>
+                            <th>Hành động</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {athletes.map((athlete, i) => (
+                            <tr key = {athlete.id}>
+                                <td>{athlete.id}</td>
+                                <td>{athlete.athleteName}</td>
+                                <td>{athlete.dateOfBirth}</td>
+                                <td>{athlete.hometown}</td>
+                                <td>{athlete.totalScoresOfCriterias}</td>
+                                <td>{athlete.grade}</td>
+                                <td>{athlete.athleteRank}</td>
+                                <td>{athlete.createAt}</td>
+                                <td>{athlete.lastModified}</td>
+                                <td>
+                                    <ButtonGroup>
+                                        <Button size="sm" color="info" onClick={() => viewAthlete(athlete.id)}>Xem</Button>
+                                        <Button size="sm" color="primary" onClick={() => editAthlete(athlete.id)} >Sửa</Button>
+                                        <Button size="sm" color="danger" onClick={() => deleteAthlete(athlete.id)} >Xóa</Button>
+                                    </ButtonGroup>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Container>
+        </div>
+
+    );
+
+}
