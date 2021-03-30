d45b52061bb7893aa155b8be070f4714c92f9633
Thuanmu <43351896+Thuanmu@users.noreply.github.com>
2021-03-30 12:34:59 +0700

    update App component

--- src/main/resources/webapp/app/src/App.jsx
+++ src/main/resources/webapp/app/src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import AthleteClassification from "./components/athlete-classification";
-import Form from "./components/factors/form";
-import Physical from "./components/factors/physical";
-import Psychophysiology from "./components/factors/psychophysiology";
-import Technical from "./components/factors/technical ";
+import AthleteClassification from "./components/classification-function/athlete-classification";
+import Athlete from "./components/athlete/athlete";
+import AthleteDetail from "./components/athlete/athlete-detail";
+import AthleteUpdate from "./components/athlete/athlete-update";
+import Form from "./components/factors/form/form";
+import FormFactorUpdate from "./components/factors/form/form-update";
+import Physical from "./components/factors/physical/physical";
+import PhysicalFactorUpdate from "./components/factors/physical/physical-update";
+import Psychophysiology from "./components/factors/psychophysiology/psychophysiology";
+import PsychophysiologyFactorUpdate from "./components/factors/psychophysiology/psychophysiology-update";
+import Technical from "./components/factors/technical/technical";
+import TechnicalFactorUpdate from "./components/factors/technical/technical-update";
 import Footer from "./components/layout/footer/footer";
 import Header from "./components/layout/header/header";
 import { Home } from "./components/layout/header/header-components";
-import Rankings from "./components/rankings";
+import Rankings from "./components/rankings/rankings";
+
 
 function App() {
   return (
@@ -19,11 +27,34 @@ function App() {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/athlete-classification" component={AthleteClassification} exact />
-        <Route path="/rankings" component={Rankings} exact />
-        <Route path="/physical" component={Physical} exact />
-        <Route path="/technical" component={Technical} exact />
-        <Route path="/psychophysiology" component={Psychophysiology} exact />
-        <Route path="/form" component={Form} exact />
+        <Route path="/rankingsList" component={Rankings} exact />
+        
+        
+        {/*form*/}
+        <Route path="/formFactors" component={Form} exact/>
+        <Route path="/formFactors/new" component={FormFactorUpdate} exact/>
+        <Route path="/formFactors/:id/edit" component={FormFactorUpdate} exact/>
+        {/* athlete */}
+        <Route path="/athletes" component={Athlete} exact />
+        <Route path="/athletes/:id/detail" component={AthleteDetail} exact />
+        <Route path="/athletes/:new" component={AthleteUpdate} exact/>
+        <Route path="/athletes/:id/edit" component={AthleteUpdate} exact/>
+        {/* technicalFactor */}
+        <Route path="/technicalFactors" component={Technical} exact />
+        {/* <Route path="/technicalFactors/:id/detail" component={TechnicalFactorDetail} exact /> */}
+        <Route path="/technicalFactors/:new" component={TechnicalFactorUpdate} exact/>
+        <Route path="/technicalFactors/:id/edit" component={TechnicalFactorUpdate} exact/>
+        {/* physicalFactor */}
+        <Route path="/physicalFactors" component={Physical} exact />
+        {/* <Route path="/physicalFactors/:id/detail" component={PhysicalFactorDetail} exact /> */}
+        <Route path="/physicalFactors/:new" component={PhysicalFactorUpdate} exact/>
+        <Route path="/physicalFactors/:id/edit" component={PhysicalFactorUpdate} exact/>
+        {/* psychophysiologyFactor */}
+        <Route path="/psychophysiologyFactors" component={Psychophysiology} exact />
+        {/* <Route path="/psychophysiologyFactors/:id/detail" component={PhysicalFactorDetail} exact /> */}
+        <Route path="/psychophysiologyFactors/:new" component={PsychophysiologyFactorUpdate} exact/>
+        <Route path="/psychophysiologyFactors/:id/edit" component={PsychophysiologyFactorUpdate} exact/>
+
         <Route component={Error} />
       </Switch>
       <Footer/>
