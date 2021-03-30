c08190b3fcd27274652769c126b7222b3b75e26b
Thuanmu <43351896+Thuanmu@users.noreply.github.com>
2021-03-30 12:02:20 +0700

    update repository 1st time

---
+++ src/main/java/com/thuanmu/traininglevelassessment/repository/AthleteClassificationRepository.java
@@ -0,0 +1,23 @@
+package com.thuanmu.traininglevelassessment.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.thuanmu.traininglevelassessment.entity.AthleteClassification;
+
+@Repository
+public interface AthleteClassificationRepository extends JpaRepository<AthleteClassification, Long> {
+	
+	@Query(value = "select ac.* from athlete_classification ac where ac.create_at = (select max(ac.create_at) from athlete_classification ac where month(ac.create_at) = ?1 and year(ac.create_at) = ?2) order by ac.athlete_rank", nativeQuery = true)
+	List<AthleteClassification> getRankingsByMonthAndYear(int month, int year);
+	
+	@Query(value = "select ac.* from athlete_classification ac where ac.create_at in (select max(ac.create_at) as lastDateOfMonth from athlete_classification ac group by month(ac.create_at)) group by ac.create_at order by ac.create_at desc", nativeQuery = true)
+	List<AthleteClassification> getAthleteClassificationByLastDateOfMonth();
+
+
+
+	
+}
