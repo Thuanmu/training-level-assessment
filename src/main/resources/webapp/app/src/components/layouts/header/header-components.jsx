71671a11e29dbf8b182abc5fc626faec980dd6af
Thuanmu <43351896+Thuanmu@users.noreply.github.com>
2021-06-29 01:21:08 +0700

    update header and footer times 2

--- src/main/resources/webapp/app/src/components/layouts/header/header-components.jsx
+++ src/main/resources/webapp/app/src/components/layouts/header/header-components.jsx
@@ -1,23 +1,38 @@
 import React from "react";
-import { Container, NavbarBrand, NavLink, Row } from "reactstrap";
-import { NavLink as Link } from 'react-router-dom';
+import { Container, NavbarBrand, NavLink, Row, Alert } from "reactstrap";
+import { NavLink as Link } from "react-router-dom";
 
-export const BrandIcon = props => (
+export const BrandIcon = (props) => (
   <div {...props} className="brand-icon">
-    <img src="../../../assets/images/TrainingLevel2.png" alt="Logo" width='100%'height='100%' />
+    <img
+      src="../../../assets/images/TrainingLevel2.png"
+      alt="Logo"
+      width="100%"
+      height="100%"
+    />
   </div>
 );
 
-export const Brand = props => (
-    <NavbarBrand href="/" className="brand-logo">
-      <span className="brand-title">TrainingLevel100m</span>
-    </NavbarBrand>
-  );
+export const Brand = (props) => (
+  <NavbarBrand href="/" className="brand-logo">
+    <span className="brand-title">TrainingLevel</span>
+  </NavbarBrand>
+);
 
-export const Home = props => (
-    <div>
-      <Container fluid>
-        <h2>Chào mừng đến với trang web đánh giá trình độ tập luyện của các vận động viên chạy 100m cấp cao </h2>
-      </Container>
-    </div>
+export const Home = (props) => (
+  <div>
+    <Container fluid>
+      <Alert color="primary">
+        <h2>Hệ thống đánh giá trình độ tập luyện của vận động viên thể thao</h2>
+        &nbsp;
+        <ul id="home-list">
+          <li>Phân loại vận động viên</li>
+          <li>Xem bảng xếp hạng vận động viên</li>
+          <li>Xem biểu đồ biểu diễn các yếu tố</li>
+          <li>Quản lý các vận động viên</li>
+          <li>Quản lý các yếu tố của các vận động viên</li>
+        </ul>
+      </Alert>
+    </Container>
+  </div>
 );
